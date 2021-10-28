3e2f4bee96dc159f7a854e724acb9e0d60539c7d
MikeDestroyer <Treil911@gmail.com>
2021-10-29 01:38:08 +0300

    Some fixes

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         prevArrow: $('#btn-left'),
         nextArrow: $('#btn-right')
     })

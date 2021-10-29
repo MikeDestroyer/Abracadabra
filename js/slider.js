96ab2a5e9891348062a63c153a9f08df6d8cafb2
MikeDestroyer <teril911@gmail.com>
2021-10-29 18:01:32 +0300

    First upload

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#slider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('#btn-left'),
-        nextArrow: $('#btn-right')
-    })
-});
